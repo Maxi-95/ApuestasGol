4d34ae641f97cab06a788915326f18d72249e2d5
Maximiliano Fonseca <maximilianofonseca19@gmail.com>
2024-01-12 17:03:17 -0300

    nuevos cambios

--- ApuestasGol/src/App.jsx
+++ ApuestasGol/src/App.jsx
@@ -35,199 +35,182 @@ function classNames(...classes) {
 function App() {
   return (
     <>
-      {/*
-        This example requires updating your template:
+      <body class="bg-gray-200">
+        <nav class="bg-white border-b border-gray-300">
+          <div class="flex justify-between items-center px-9">
+            <button id="menuBtn">
+              <i class="fas fa-bars text-cyan-500 text-lg"></i>
+            </button>
 
-        ```
-        <html class="h-full bg-gray-100">
-        <body class="h-full">
-        ```
-      */}
-      <div className="min-h-full">
-        <Disclosure as="nav" className="bg-gray-800">
-          {({ open }) => (
-            <>
-              <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                <div className="flex h-16 items-center justify-between">
-                  <div className="flex items-center">
-                    <div className="flex-shrink-0">
-                      <img
-                        className="h-8 w-8"
-                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                        alt="Your Company"
-                      />
-                    </div>
-                    <div className="hidden md:block">
-                      <div className="ml-10 flex items-baseline space-x-4">
-                        {navigation.map((item) => (
-                          <a
-                            key={item.name}
-                            href={item.href}
-                            className={classNames(
-                              item.current
-                                ? "bg-gray-900 text-white"
-                                : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                              "rounded-md px-3 py-2 text-sm font-medium"
-                            )}
-                            aria-current={item.current ? "page" : undefined}
-                          >
-                            {item.name}
-                          </a>
-                        ))}
-                      </div>
-                    </div>
-                  </div>
-                  <div className="hidden md:block">
-                    <div className="ml-4 flex items-center md:ml-6">
-                      <button
-                        type="button"
-                        className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                      >
-                        <span className="absolute -inset-1.5" />
-                        <span className="sr-only">View notifications</span>
-                        <BellIcon className="h-6 w-6" aria-hidden="true" />
-                      </button>
+            <div class="ml-1">
+              <img
+                src="https://www.emprenderconactitud.com/img/POC%20WCS%20(1).png"
+                alt="logo"
+                class="h-20 w-28"
+              />
+            </div>
 
-                      {/* Profile dropdown */}
-                      <Menu as="div" className="relative ml-3">
-                        <div>
-                          <Menu.Button className="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                            <span className="absolute -inset-1.5" />
-                            <span className="sr-only">Open user menu</span>
-                            <img
-                              className="h-8 w-8 rounded-full"
-                              src={user.imageUrl}
-                              alt=""
-                            />
-                          </Menu.Button>
-                        </div>
-                        <Transition
-                          as={Fragment}
-                          enter="transition ease-out duration-100"
-                          enterFrom="transform opacity-0 scale-95"
-                          enterTo="transform opacity-100 scale-100"
-                          leave="transition ease-in duration-75"
-                          leaveFrom="transform opacity-100 scale-100"
-                          leaveTo="transform opacity-0 scale-95"
-                        >
-                          <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                            {userNavigation.map((item) => (
-                              <Menu.Item key={item.name}>
-                                {({ active }) => (
-                                  <a
-                                    href={item.href}
-                                    className={classNames(
-                                      active ? "bg-gray-100" : "",
-                                      "block px-4 py-2 text-sm text-gray-700"
-                                    )}
-                                  >
-                                    {item.name}
-                                  </a>
-                                )}
-                              </Menu.Item>
-                            ))}
-                          </Menu.Items>
-                        </Transition>
-                      </Menu>
-                    </div>
-                  </div>
-                  <div className="-mr-2 flex md:hidden">
-                    {/* Mobile menu button */}
-                    <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                      <span className="absolute -inset-0.5" />
-                      <span className="sr-only">Open main menu</span>
-                      {open ? (
-                        <XMarkIcon
-                          className="block h-6 w-6"
-                          aria-hidden="true"
-                        />
-                      ) : (
-                        <Bars3Icon
-                          className="block h-6 w-6"
-                          aria-hidden="true"
-                        />
-                      )}
-                    </Disclosure.Button>
-                  </div>
+            <div class="space-x-4">
+              <button>
+                <i class="fas fa-bell text-cyan-500 text-lg"></i>
+              </button>
+
+              <button>
+                <i class="fas fa-user text-cyan-500 text-lg"></i>
+              </button>
+            </div>
+          </div>
+        </nav>
+
+        <div
+          id="sideNav"
+          class="lg:block hidden bg-white w-64 h-screen fixed rounded-none border-none"
+        >
+          <div class="p-4 space-y-4">
+            <a
+              href="#"
+              aria-label="dashboard"
+              class="relative px-4 py-3 flex items-center space-x-4 rounded-lg text-white bg-gradient-to-r from-sky-600 to-cyan-400"
+            >
+              <i class="fas fa-home text-white"></i>
+              <span class="-mr-1 font-medium">Inicio</span>
+            </a>
+
+            <a
+              href="#"
+              class="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-500 group"
+            >
+              <i class="fas fa-wallet"></i>
+              <span>Billetera</span>
+            </a>
+            <a
+              href="#"
+              class="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-500 group"
+            >
+              <i class="fas fa-exchange-alt"></i>
+              <span>Transacciones</span>
+            </a>
+            <a
+              href="#"
+              class="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-500 group"
+            >
+              <i class="fas fa-user"></i>
+              <span>Mi cuenta</span>
+            </a>
+            <a
+              href="#"
+              class="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-500 group"
+            >
+              <i class="fas fa-sign-out-alt"></i>
+              <span>Cerrar sesión</span>
+            </a>
+          </div>
+        </div>
+
+        <div class="lg:ml-64 lg:pl-4 lg:flex lg:flex-col lg:w-75% mt-5 mx-2">
+          <div class="bg-white rounded-full border-none p-3 mb-4 shadow-md">
+            <div class="flex items-center">
+              <i class="px-3 fas fa-search ml-1"></i>
+              <input
+                type="text"
+                placeholder="Buscar..."
+                class="ml-3 focus:outline-none w-full"
+              />
+            </div>
+          </div>
+
+          <div class="lg:flex gap-4 items-stretch">
+            <div class="bg-white md:p-2 p-6 rounded-lg border border-gray-200 mb-4 lg:mb-0 shadow-md lg:w-[35%]">
+              <div class="flex justify-center items-center space-x-5 h-full">
+                <div>
+                  <p>Saldo actual</p>
+                  <h2 class="text-4xl font-bold text-gray-600">50.365</h2>
+                  <p>25.365 $</p>
+                </div>
+                <img
+                  src="https://www.emprenderconactitud.com/img/Wallet.png"
+                  alt="wallet"
+                  class="h-24 md:h-20 w-38"
+                />
+              </div>
+            </div>
+
+            <div class="bg-white p-4 rounded-lg xs:mb-4 max-w-full shadow-md lg:w-[65%]">
+              <div class="flex flex-wrap justify-between h-full">
+                <div class="flex-1 bg-gradient-to-r from-cyan-400 to-cyan-600 rounded-lg flex flex-col items-center justify-center p-4 space-y-2 border border-gray-200 m-2">
+                  <i class="fas fa-hand-holding-usd text-white text-4xl"></i>
+                  <p class="text-white">Depositar</p>
+                </div>
+
+                <div class="flex-1 bg-gradient-to-r from-cyan-400 to-cyan-600 rounded-lg flex flex-col items-center justify-center p-4 space-y-2 border border-gray-200 m-2">
+                  <i class="fas fa-exchange-alt text-white text-4xl"></i>
+                  <p class="text-white">Transferir</p>
+                </div>
+
+                <div class="flex-1 bg-gradient-to-r from-cyan-400 to-cyan-600 rounded-lg flex flex-col items-center justify-center p-4 space-y-2 border border-gray-200 m-2">
+                  <i class="fas fa-qrcode text-white text-4xl"></i>
+                  <p class="text-white">Canjear</p>
                 </div>
               </div>
-
-              <Disclosure.Panel className="md:hidden">
-                <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
-                  {navigation.map((item) => (
-                    <Disclosure.Button
-                      key={item.name}
-                      as="a"
-                      href={item.href}
-                      className={classNames(
-                        item.current
-                          ? "bg-gray-900 text-white"
-                          : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                        "block rounded-md px-3 py-2 text-base font-medium"
-                      )}
-                      aria-current={item.current ? "page" : undefined}
-                    >
-                      {item.name}
-                    </Disclosure.Button>
-                  ))}
-                </div>
-                <div className="border-t border-gray-700 pb-3 pt-4">
-                  <div className="flex items-center px-5">
-                    <div className="flex-shrink-0">
-                      <img
-                        className="h-10 w-10 rounded-full"
-                        src={user.imageUrl}
-                        alt=""
-                      />
-                    </div>
-                    <div className="ml-3">
-                      <div className="text-base font-medium leading-none text-white">
-                        {user.name}
-                      </div>
-                      <div className="text-sm font-medium leading-none text-gray-400">
-                        {user.email}
-                      </div>
-                    </div>
-                    <button
-                      type="button"
-                      className="relative ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                    >
-                      <span className="absolute -inset-1.5" />
-                      <span className="sr-only">View notifications</span>
-                      <BellIcon className="h-6 w-6" aria-hidden="true" />
-                    </button>
-                  </div>
-                  <div className="mt-3 space-y-1 px-2">
-                    {userNavigation.map((item) => (
-                      <Disclosure.Button
-                        key={item.name}
-                        as="a"
-                        href={item.href}
-                        className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
-                      >
-                        {item.name}
-                      </Disclosure.Button>
-                    ))}
-                  </div>
-                </div>
-              </Disclosure.Panel>
-            </>
-          )}
-        </Disclosure>
-
-        <header className="bg-white shadow">
-          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold tracking-tight text-gray-900">
-              Dashboard
-            </h1>
+            </div>
           </div>
-        </header>
-        <main>
-          <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-            {/* Your content */}
+
+          <div class="bg-white rounded-lg p-4 shadow-md my-4">
+            <table class="table-auto w-full">
+              <thead>
+                <tr>
+                  <th class="px-4 py-2 text-left border-b-2 w-full">
+                    <h2 class="text-ml font-bold text-gray-600">
+                      Transacciones
+                    </h2>
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr class="border-b w-full">
+                  <td class="px-4 py-2 text-left align-top w-1/2">
+                    <div>
+                      <h2>Comercio</h2>
+                      <p>24/07/2023</p>
+                    </div>
+                  </td>
+                  <td class="px-4 py-2 text-right text-cyan-500 w-1/2">
+                    <p>
+                      <span>150$</span>
+                    </p>
+                  </td>
+                </tr>
+                <tr class="border-b w-full">
+                  <td class="px-4 py-2 text-left align-top w-1/2">
+                    <div>
+                      <h2>Comercio</h2>
+                      <p>24/06/2023</p>
+                    </div>
+                  </td>
+                  <td class="px-4 py-2 text-right text-cyan-500 w-1/2">
+                    <p>
+                      <span>15$</span>
+                    </p>
+                  </td>
+                </tr>
+                <tr class="border-b w-full">
+                  <td class="px-4 py-2 text-left align-top w-1/2">
+                    <div>
+                      <h2>Comercio</h2>
+                      <p>02/05/2023</p>
+                    </div>
+                  </td>
+                  <td class="px-4 py-2 text-right text-cyan-500 w-1/2">
+                    <p>
+                      <span>50$</span>
+                    </p>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
-        </main>
-      </div>
+        </div>
+      </body>
     </>
   );
 }
