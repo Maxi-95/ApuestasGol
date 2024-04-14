64cad6cf30b0f9db736b24708d75735120144171
Maximiliano Fonseca <maximilianofonseca19@gmail.com>
2024-04-14 17:24:24 -0300

    estructura: nav, home

--- ApuestasGol/src/App.jsx
+++ ApuestasGol/src/App.jsx
@@ -61,51 +61,7 @@ function App() {
             </div>
           </div>
         </nav>
-
-        <div
-          id="sideNav"
-          class="lg:block hidden bg-white w-64 h-screen fixed rounded-none border-none"
-        >
-          <div class="p-4 space-y-4">
-            <a
-              href="#"
-              aria-label="dashboard"
-              class="relative px-4 py-3 flex items-center space-x-4 rounded-lg text-white bg-gradient-to-r from-sky-600 to-cyan-400"
-            >
-              <i class="fas fa-home text-white"></i>
-              <span class="-mr-1 font-medium">Inicio</span>
-            </a>
-
-            <a
-              href="#"
-              class="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-500 group"
-            >
-              <i class="fas fa-wallet"></i>
-              <span>Billetera</span>
-            </a>
-            <a
-              href="#"
-              class="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-500 group"
-            >
-              <i class="fas fa-exchange-alt"></i>
-              <span>Transacciones</span>
-            </a>
-            <a
-              href="#"
-              class="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-500 group"
-            >
-              <i class="fas fa-user"></i>
-              <span>Mi cuenta</span>
-            </a>
-            <a
-              href="#"
-              class="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-500 group"
-            >
-              <i class="fas fa-sign-out-alt"></i>
-              <span>Cerrar sesión</span>
-            </a>
-          </div>
-        </div>
+        <BarraIzquierda></BarraIzquierda>
 
         <div class="lg:ml-64 lg:pl-4 lg:flex lg:flex-col lg:w-75% mt-5 mx-2">
           <div class="bg-white rounded-full border-none p-3 mb-4 shadow-md">
